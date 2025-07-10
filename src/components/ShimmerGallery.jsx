9ec2439c8e1afae36e98d46a0f53a9a38fe8debb
metatony <ibegbulam.ua@gmail.com>
2025-07-10 02:58:58 +0100

    feat: enhance ShimmerGallery and ShimmerPost components for responsive design; add ShimmerSimpleGallery for mobile view and adjust column settings

--- src/components/ShimmerGallery.jsx
+++ src/components/ShimmerGallery.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { ShimmerFeaturedGallery } from "react-shimmer-effects";
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
+
 
 
 const ShimmerGallery = () => {
   return (
-    <div className='px-5 lg:px-10 mt-10'>
-        <ShimmerFeaturedGallery row={2} col={2} card frameHeight={600} />
-    </div>
+    <>
+      <div className='md:hidden px-5 mt-10'>
+        <ShimmerSimpleGallery col={2} row={2} card imageHeight={300} caption />
+      </div>
+
+      <div className='hidden md:block lg:px-10 mt-10'>
+          <ShimmerFeaturedGallery row={2} col={2} card frameHeight={600} />
+      </div>
+    
+    </>
+
   )
 }
 
--- src/components/ShimmerPost.jsx
+++ src/components/ShimmerPost.jsx
@@ -4,10 +4,19 @@ import { ShimmerPostList } from "react-shimmer-effects";
 
 const ShimmerPost = () => {
   return (
-    <div className='px-5 lg:px-10 mt-10'>
-        <ShimmerPostList postStyle="STYLE_FOUR" col={3} row={1} gap={30} />
-    </div>
+    <>
+      <div className='md:hidden px-5 mt-10'>
+          <ShimmerPostList postStyle="STYLE_FOUR" col={2} row={1} gap={15} />
+      </div>
+
+      <div className='hidden md:block lg:px-10 mt-10'>
+          <ShimmerPostList postStyle="STYLE_FOUR" col={3} row={1} gap={30} />
+      </div>
     
+    
+    
+    </>
+
 
   )
 }
