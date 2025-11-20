7c463762e3864044c9170b2a72ce68f849530151
metatony <ibegbulam.ua@gmail.com>
2025-11-20 00:33:29 +0000

    fix: add @tailwindcss/aspect-ratio dependency and configure plugin in Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import aspectRatio from "@tailwindcss/aspect-ratio";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [aspectRatio],
+};
